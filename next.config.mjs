6a7d2b74c461b02279ee6886a841cf92cab625ec
challavenkataramana <challavenkataramana@gmail.com>
2025-04-20 09:23:15 +0530

    Refactor Next.js configuration to enable export output and remove commented code

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,7 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     output: 'export', 
-// };
-
-// export default nextConfig;
-
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {
-      unoptimized: true,
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+    output: 'export', 
+};
+
+export default nextConfig;
+
